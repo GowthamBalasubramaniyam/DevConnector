002bbbd251759272d96f3796900941cc0662571c
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2026-05-07 13:36:02 +0530

    Allowing the react app to use APIs

--- devconnector-api/src/main/java/com/devconnector/config/WebConfig.java
+++ devconnector-api/src/main/java/com/devconnector/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("*") // Allow the React app
+                .allowedOrigins("https://dev-verse-coral.vercel.app/") // Allow the React app
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
